21b27d34099557bdc29eff2daf1d53c0a299c1a2
zeke <zzhmisanmia@outlook.com>
2020-05-27 17:21:20 +0800

    初始化

---
+++ src/main/java/edu/scujcc/pircloud/PircloudApplication.java
@@ -0,0 +1,13 @@
+package edu.scujcc.pircloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PircloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PircloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/scujcc/pircloud/PircloudApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.scujcc.pircloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PircloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
